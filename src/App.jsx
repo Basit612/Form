f4b2cf1f489129ca0f4991380d902772855e0e49
Basit612 <sheikhabdulbasit612@gmail.com>
2024-03-22 01:20:41 +0600

    Reusable Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-
-import './App.css'
-import HookForm from './componentas/HookForm/HookForm'
+import { data } from "autoprefixer";
+import "./App.css";
+import ReusableForm from "./componentas/ReusableForm/ReusableForm";
+// import HookForm from './componentas/HookForm/HookForm'
 // import RefForm from './componentas/RefForm/RefForm'
 // import StatefulForm from './componentas/SimpleForm/StatefulForm/StatefulForm'
 // import SimpleForm from './componentas/SimpleForm/SimpleForm'
 
-
 function App() {
-
-
+  const handleSingUpSubmit = data =>{
+      console.log('sing up',data)
+     }
+    
+     const handleUpdate= data =>{
+     console.log('profile update',data)
+    }
+     
   return (
     <>
-    
-      <h1 className='text-5xl'>Form</h1>
-     {/* <SimpleForm></SimpleForm> */}
-     {/* <StatefulForm></StatefulForm> */}
-     {/* <RefForm></RefForm> */}
-     <HookForm></HookForm>
+      <h1 className="text-5xl">Form</h1>
+      {/* <SimpleForm></SimpleForm> */}
+      {/* <StatefulForm></StatefulForm> */}
+      {/* <RefForm></RefForm> */}
+      {/* <HookForm></HookForm> */}
+      <ReusableForm formTitle={"sing up"} handleSubmit={handleSingUpSubmit}>
+         <div>
+          <h1>Sing up</h1>
+          <p>please sing up</p>
+        </div>
+        </ReusableForm>
+      <ReusableForm
+        formTitle={"Profile Update"}
+        handleSubmit={handleUpdate}
+        submitbutton="Update"
+      >
+        <div>
+          <h1>profile</h1>
+          <p>sing up profile</p>
+        </div>
+      </ReusableForm>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
